b5c0a9e68ba6700d0af3490b3d59abc7bec97fb8
Sam Richard <snugug@google.com>
2021-03-19 11:12:16 -0400

    Add changes from PWA03 - IndexedDB

--- js/main.js
+++ js/main.js
@@ -14,6 +14,8 @@
  limitations under the License.
  */
 
+import { openDB } from 'idb';
+
 // Need to use this WMR syntax to properly compile the service worker.
 // If you compile your service worker in another way, you can use the URL to it
 // directly in navigator.serviceWorker.register
@@ -34,6 +36,13 @@ if ('serviceWorker' in navigator) {
 }
 
 window.addEventListener('DOMContentLoaded', async () => {
+  // Set up the database
+  const db = await openDB('settings-store', 1, {
+    upgrade(db) {
+      db.createObjectStore('settings');
+    },
+  });
+
   // Set up the editor
   const { Editor } = await import('./app/editor.js');
   const editor = new Editor(document.body);
@@ -42,8 +51,24 @@ window.addEventListener('DOMContentLoaded', async () => {
   const { Menu } = await import('./app/menu.js');
   new Menu(document.querySelector('.actions'), editor);
 
+  // Save content to database on edit
+  editor.onUpdate(async (content) => {
+    await db.put('settings', content, 'content');
+  });
+
   // Set the initial state in the editor
   const defaultText = `# Welcome to PWA Edit!\n\nTo leave the editing area, press the \`esc\` key, then \`tab\` or \`shift+tab\`.`;
 
-  editor.setContent(defaultText);
+  editor.setContent((await db.get('settings', 'content')) || defaultText);
+
+  // Set up night mode toggle
+  const { NightMode } = await import('./app/night-mode.js');
+  new NightMode(
+    document.querySelector('#mode'),
+    async (mode) => {
+      editor.setTheme(mode);
+      await db.put('settings', mode, 'night-mode');
+    },
+    await db.get('settings', 'night-mode'),
+  );
 });
