c7ac8a33ebf3fe3e6ee1dfba38321d608d4e80d0
Sam Richard <snugug@google.com>
2021-03-19 11:16:38 -0400

    Add changes from PWA05 - Empowering Your PWA

--- js/lib/actions.js
+++ js/lib/actions.js
@@ -14,36 +14,167 @@
  limitations under the License.
  */
 
+import { openDB } from 'idb';
+
 export class Actions {
-  constructor() {}
+  constructor() {
+    this.db = null;
+
+    // Get the database and current open file handler
+    openDB('settings-store').then(async (db) => {
+      this.db = db;
+      const file = await db.get('settings', 'handler');
+
+      if (file) {
+        document.title = `${file.name} | PWA Edit`;
+        this.handler = file;
+      }
+    });
+
+    // Get launched files
+    if ('launchQueue' in window) {
+      launchQueue.setConsumer((params) => {
+        if (!params.files.length) return;
+
+        for (const handler of params.files) {
+          this.open(handler);
+        }
+      });
+    }
+
+    // Close the preview window when this window closes
+    window.addEventListener('beforeunload', () => {
+      if (this.previewWindow) {
+        this.previewWindow.close();
+      }
+    });
+  }
 
   /**
    * Function to call when the open button is triggered
    */
-  async open() {}
+  async open(launchHandler) {
+    try {
+      let handler;
+      if (launchHandler instanceof FileSystemFileHandle) {
+        handler = launchHandler;
+      } else {
+        [handler] = await window.showOpenFilePicker({
+          types: [
+            {
+              description: 'Markdown files',
+              accept: {
+                'text/markdown': ['.md', '.markdown'],
+              },
+            },
+          ],
+        });
+      }
+
+      document.title = `${handler.name} | PWA Edit`;
+
+      const file = await handler.getFile();
+      const content = await file.text();
+
+      this.handler = handler;
+      this.editor.setContent(content);
+      await this.db.put('settings', handler, 'handler');
+    } catch (e) {
+      console.error(e);
+    }
+  }
 
   /**
    * Function to call when the save button is triggered
    */
-  async save() {}
+  async save() {
+    const handler = this.handler || (await this.db.get('settings', 'handler'));
+
+    if (!handler) {
+      await this.saveAs();
+    } else {
+      try {
+        const writable = await handler.createWritable();
+        await writable.write(this.editor.content());
+        await writable.close();
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  }
 
   /**
    * Function to call when the duplicate/save as button is triggered
    */
-  async saveAs() {}
+  async saveAs() {
+    try {
+      const handler = await window.showSaveFilePicker({
+        types: [
+          {
+            description: 'Markdown file',
+            accept: {
+              'text/markdown': ['.md'],
+            },
+          },
+        ],
+      });
+
+      this.handler = handler;
+      await this.db.put('settings', handler, 'handler');
+      await this.save();
+    } catch (e) {
+      console.error(e);
+    }
+  }
 
   /**
    * Reset the editor and file handler
    */
-  async reset() {}
+  async reset() {
+    this.editor.setContent('');
+    this.handler = null;
+    document.title = 'PWA Edit';
+    await this.db.delete('settings', 'handler');
+  }
 
   /**
    * Function to call when the preview button is triggered
    */
-  async preview() {}
+  async preview() {
+    if (this.previewWindow) {
+      this.previewWindow.close();
+      this.previewWindow = null;
+      return;
+    }
+
+    const options = 'menubar=0,toolbar=0,status=0,location=0';
+    const { screens } = await window.getScreens();
+
+    // Find the primary screen
+    const screen = screens.find((s) => s.isPrimary);
+
+    // Get it's available width
+    const width = screen.availWidth / 2;
+
+    // Build a window from the information
+    this.previewWindow = window.open('/preview', 'Markdown preview', `${options},left=${width},top=${screen.availTop},height=${screen.availHeight},width=${width}`);
+  }
 
   /**
    * Function to call when the focus button is triggered
    */
-  async focus() {}
+  async focus() {
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+      if (this.wakeLock) {
+        this.wakeLock.release();
+        this.wakeLock = null;
+      }
+    } else {
+      if ('wakeLock' in navigator) {
+        this.wakeLock = await navigator.wakeLock.request();
+      }
+      await document.body.requestFullscreen();
+    }
+  }
 }
